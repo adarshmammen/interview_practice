2ac66e08cf0096380c22a48edff4fb05224729ef
Adarsh <adarsh.mammen@gmail.com>
2018-02-01 02:12:10 -0500

    added shortest path

--- graph_find_path.py
+++ graph_find_path.py
@@ -38,6 +38,30 @@ def find_all_path(graph, start, end, path = []):
 				paths.append(newpaths) 
 	return paths
 
+def shortest_path(graph, start, end, path = []):
+	# backtracking algorithm
+	path = path + [start]
+	if start == end:
+		return [path]
+	if start not in graph:
+		return None
+	paths = []
+	for node in graph[start]:
+		if node not in path: #no cycles
+			#partial solution
+			newpaths = find_path(graph,node,end,path)
+		if newpaths:
+			for newpath in newpaths:
+				paths.append(newpaths) 
+	if len(paths)>1:
+		len_li = []
+		for li in paths:
+			len_li.append(len(li))
+		return paths[len_li.index(min(len_li))]
+
+	return paths
+
 if __name__ == "__main__":
 	print(find_path(test,'a','f'))
 	print(find_all_path(test,'a','d'))
+	print(shortest_path(test,'a','d'))
