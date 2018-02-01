397992bdc732c7d2973629510b6b98793a2444a5
Adarsh <adarsh.mammen@gmail.com>
2018-02-01 01:15:58 -0500

    adding graph path problem

---
+++ graph_find_path.py
@@ -0,0 +1,24 @@
+test = {'a':['b','c'],
+		'b':['c','d'],
+		'c':['b','d','e'],
+		'd':['b','c'],
+		'e':['c'],
+		'f':[]
+}
+
+def find_path(graph, start, end, path = []):
+	# backtracking algorithm
+	if start == end:
+		return path
+	if start not in graph:
+		return None
+	path = path + [start]
+	for node in graph[start]:
+		if node not in path: #no cycles
+			#partial solution
+			newpath = find_path(graph,node,end,path)
+			if newpath: return newpath
+	return None
+
+if __name__ == "__main__":
+	print(find_path(test,'a','e'))
