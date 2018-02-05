891601c2d76dff0cfa1568c447a49bacfdda9a19
Adarsh Verghese <adarsh.mammen@gmail.com>
2018-02-04 20:56:45 -0500

    Create add_lists.py

---
+++ add_lists.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = []
+        v1,v2 = [],[]
+        p1 = l1
+        p2 = l2
+        ret = ListNode(0)
+        carry = 0
+        while p1 != None or p2 != None or carry != 0:
+            add1,add2 = 0,0
+            if p1 == None and p2 == None:
+                add1 = 0
+                add2 = 0
+            elif p1 == None:
+                add1 = 0
+                add2 = p2.val
+                p2 = p2.next
+            elif p2 == None:
+                add1 = p1.val
+                add2 = 0
+                p1 = p1.next
+            else:
+                add1 = p1.val
+                add2 = p2.val
+                p1 = p1.next
+                p2 = p2.next
+            temp = add1 + add2 + carry
+            carry = temp/10
+            res.append(temp%10)
+        
+        return res
+        
